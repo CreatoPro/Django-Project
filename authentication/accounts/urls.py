cdfd03aaba9b58dffbe7f06523fb1e99cbbb8299
CreatoPro <creatoproco@gmail.com>
2021-12-11 23:45:35 +0530

    authentication

---
+++ authentication/accounts/urls.py
@@ -0,0 +1,17 @@
+from rest_framework import views
+from .views import RegisterAPI, LoginAPI
+from django.urls import path, include
+from knox import views as knox_views
+from .views import ChangePasswordView
+
+
+urlpatterns = [
+    path('api/register/', RegisterAPI.as_view(), name='register'),
+    path('api/login/', LoginAPI.as_view(), name='login'),
+    path('api/logout/', knox_views.LogoutView.as_view(), name='logout'),
+    path('api/logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
+    path('api/change-password/', ChangePasswordView.as_view(), name='change-password'),
+    path('api/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
+    ]
+
+
